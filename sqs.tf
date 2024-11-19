057aacb1bc3c6da765d1607403e4f42c77cae487
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2024-11-19 01:05:16 -0800

    added tf files

---
+++ sqs.tf
@@ -0,0 +1,10 @@
+
+resource "aws_sqs_queue" "reversal_queue" {
+  name                        = "ReversalProcessingQueue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+
+  tags = {
+    Name = "ReversalProcessingQueue"
+  }
+}
